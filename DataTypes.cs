4895dd64d0c3aa7c8861475051e5f8e1404c97c1
Guo Jiayan <guo_jiayan@sp.edu.sg>
2019-11-07 10:10:16 +0800

    This is my first commit

---
+++ DataTypes.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DataTypes : MonoBehaviour {
+	public byte redColor;
+	public short temperature;
+	public char letter = 'x';
+    public string s = "xcvbn?";
+
+	void Start () {
+		Debug.Log(redColor);
+		Debug.Log(temperature);
+		Debug.Log(letter);
+	}
+}
